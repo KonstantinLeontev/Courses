fac95cc073ccda1edd94e4f30bfd95fac12bb417
Konstantin <KonstantinLeontev@users.noreply.github.com>
2018-03-03 23:39:12 +0300

    Modified Remove() and added FindMax() method to OrderedBinaryTree class

--- C++PrimerMailRu/OrderedBinaryTree/Source.cpp
+++ C++PrimerMailRu/OrderedBinaryTree/Source.cpp
@@ -36,7 +36,12 @@ private:
 	void Copy(Node *curr);
 	// recursive function for AddElement() method
 	Node *Insert(Node *curr, const T &newKey);
-
+	// it was part of RemoveElement() at first...
+	Node *Find(Node *curr, const T &key);
+	// this method is a part of next Remove()
+	Node *FindMax(Node *curr);
+	// recursive function to use in public RemoveElement() method
+	Node *Remove(Node *curr, const T &key);
 
 	Node *m_pHead;
 };
@@ -46,7 +51,7 @@ template <typename T> OrderedBinaryTree<T>::OrderedBinaryTree(const OrderedBinar
 	Copy(m_pHead);
 }
 
-template <typename T> void OrederedBinaryTree<T>::Copy(Node *curr) {
+template <typename T> void OrderedBinaryTree<T>::Copy(Node *curr) {
 	// if there are any nodes for copy
 	if (curr != NULL) {
 		// add current key value to this tree
@@ -84,6 +89,85 @@ typename OrderedBinaryTree<T>::Node* OrderedBinaryTree<T>::Insert(Node *curr, co
 	}
 	// after receiving the base case unwind all subtree back and return first head pointer
 	return curr;
+}
+
+template <typename T>
+typename OrderedBinaryTree<T>::Node* OrderedBinaryTree<T>::Find(Node *curr, const T &key) {
+	// base case for empty tree
+	if (curr == NULL) {
+		return NULL;
+	}
+	// if the key is found
+	else if (curr->m_Key == key) {
+		return curr;
+	}
+	// otherwise go to the left
+	else if (curr->m_Key > key) {
+		curr = Find(curr->m_pLeft, key);
+	}
+	// or go to the left side
+	else {
+		curr = Find(curr->m_pRight, key);
+	}
+}
+
+template <typename T>
+typename OrderedBinaryTree<T>::Node* OrderedBinaryTree<T>::FindMax(Node *curr) {
+	if (curr == NULL) {
+		return NULL;
+	}
+	// check only right side
+	if (curr->m_pRight == NULL) {
+		return curr;
+	}
+	return FindMax(curr->m_pRight);
+}
+
+template <typename T>
+typename OrderedBinaryTree<T>::Node* OrderedBinaryTree<T>::Remove(Node *curr, const T &key) {
+	// base case for empty tree
+	if (curr == NULL) {
+		return NULL;
+	}
+	// if key is found
+	else if (curr->m_Key == key) {
+		// there are not any left children
+		if (curr->m_pLeft == NULL) {
+			Node *temp = curr->m_pRight;
+			delete curr;
+			return temp;
+		}
+		// only right children case
+		if (curr->m_pRight == NULL) {
+			Node *temp = curr->m_pLeft;
+			delete curr;
+			return temp;
+		}
+		// otherwise find max value on the left
+		Node *maxNode = FindMax(curr->m_pLeft);
+		// point max to the left subtree
+		maxNode->m_pLeft = curr->m_pLeft;
+		// point max to the right subtree
+		maxNode->m_pRight = curr->m_pRight;
+		// it's time to delete current node
+		delete curr;
+		curr = NULL;
+		return maxNode;
+	}
+	// go to the left
+	else if (curr->m_Key > key) {
+		curr->m_pLeft = Remove(curr->m_pLeft, key);
+	}
+	// otherwise go to the right
+	else {
+		curr->m_pRight = Remove(curr->m_pRight, key);
+	}
+	return curr;
+}
+
+template <typename T> bool OrderedBinaryTree<T>::RemoveElement(const T &key) {
+	// use private recursive method, that returns new tree
+	m_pHead = Remove(m_pHead, curr);
 }
 
 template <typename T> OrderedBinaryTree<T>::readTree() {
