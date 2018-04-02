0e3d0f3bdacf04fa838651e7956a95eb501f21b9
Konstantin <KonstantinLeontev@users.noreply.github.com>
2018-04-02 23:35:38 +0300

    Base function is complited exept throw-catch

--- C++PrimerMailRu/ComplexNumber/ComplexNumberSource.cpp
+++ C++PrimerMailRu/ComplexNumber/ComplexNumberSource.cpp
@@ -4,24 +4,69 @@
 template <typename R, typename I>
 class Complex {
 public:
-	Complex() : m_Real(0), m_Imaginary(0) {};
-	Complex(const Complex &other);
-	~Complex();
+	Complex() : m_Real{}, m_Imaginary{} {} // Value initialization (since C++ 11).
+	Complex(R real, I imaginary) : m_Real(real), m_Imaginary(imaginary){} // With arguments.
+	Complex(const Complex &rhs) : m_Real(rhs.m_Real), m_Imaginary(rhs.m_Imaginary) {}
+	~Complex() {}
 
-	void SetReal(const R &real);
-	void SetImaginary(const I &imaginary);
+	void SetReal(const R &real) { m_Real = real; }
+	void SetImaginary(const I &imaginary) { m_Imaginary = imaginary; }
 
-	void Add(const Complex &other);
-	void Subtract(const Complex &other);
-	void Product(const Complex &other);
+	Complex Add(const Complex &rhs) { m_Real += rhs.m_Real; m_Imaginary += rhs.m_Imaginary; }
+	Complex Subtract(const Complex &rhs) { m_Real -= rhs.m_Real; m_Imaginary -= rhs.m_Imaginary; }
+	Complex Product(const Complex &rhs) { m_Real *= rhs.m_Real; m_Imaginary *= rhs.m_Imaginary; }
 	void ScreenOutput() const;
 
-	// Operators overloading TODO.
+	// Operators overloading
+	Complex operator+(const Complex &rhs);
+	Complex operator-(const Complex &rhs);
+	Complex operator*(const Complex &rhs);
+	Complex& operator=(const Complex &rhs);
+	template <typename V, typename W> friend std::ostream& operator<<(std::ostream &os, const Complex<V, W> &rhs);
 
 private:
 	R m_Real;
 	I m_Imaginary;
 };
+
+template <typename R, typename I>
+Complex<R, I> Complex<R, I>::operator+(const Complex &rhs) {
+	// Return value optimization - omits copy constructor since return value is a prvalue.
+	return Complex(m_Real + rhs.m_Real, m_Imaginary + rhs.m_Imaginary);
+}
+
+template <typename R, typename I>
+Complex<R, I> Complex<R, I>::operator-(const Complex &rhs) {
+	// RVO optimization.
+	return Complex(m_Real - rhs.m_Real, m_Imaginary - rhs.m_Imaginary);
+}
+
+template <typename R, typename I>
+Complex<R, I> Complex<R, I>::operator*(const Complex &rhs) {
+	return Complex(m_Real * rhs.m_Real, m_Imaginary * rhs.m_Imaginary);
+}
+
+template <typename R, typename I>
+Complex<R, I>& Complex<R, I>::operator=(const Complex &rhs) {
+	if (this == rhs) {
+		return this;
+	}
+	m_Real = rhs.m_Real;
+	m_Imaginary = rhs.m_Imaginary;
+	return this;
+}
+
+template <typename V, typename W>
+std::ostream& operator<<(std::ostream &os, const Complex<V, W> &rhs) {
+	os << '(' << rhs.m_Real << ',' << rhs.m_Imaginary << ')';
+	return os;
+}
+
+template <typename R, typename I>
+void Complex<R, I>::ScreenOutput() const {
+	std::cout << '(' << m_Real << ',' << m_Imaginary << ')';
+}
+
 
 // Read formatted input to vector.
 template <typename R, typename I>
@@ -33,21 +78,32 @@ void ReadToVector(std::vector<Complex<R, I>> &vec, Complex<R, I> &cNum, char &te
 		cNum.SetReal(real);
 		cNum.SetImaginary(imaginary);
 		// Add complex number to the vector.
-		vec1.push_back(cNum);
+		vec.push_back(cNum);
 	}
 }
 
 template <typename R, typename I>
 void AddVectors(
-	const std::vector<Complex<R, I>> &vec1,
+	std::vector<Complex<R, I>> &vec1,
 	const std::vector<Complex<R, I>> &vec2,
 	std::vector<Complex<R,I>> &vec3) {
-	// TODO
+	// Get the size of the vector.
+	int vSize = vec1.size();
+	// Go through the vectors
+	for (int i = 0; i < vSize; i++) {
+		// Perform addition of corresponding components and add result to vec3.
+		vec3.push_back(vec1[i] + vec2[i]);
+	}
 }
 
 template <typename R, typename I>
-void PrintVector(std::vector<Complex<R, I>> &vec) {
-	// TODO
+void PrintVector(const std::vector<Complex<R, I>> &vec) {
+	int vSize = vec.size();
+	std::cout << '[';
+	for (int i = 0; i < vSize; i++) {
+		std::cout << vec[i];
+	}
+	std::cout << ']';
 }
 
 int main() {
